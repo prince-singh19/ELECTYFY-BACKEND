f791e9cdb87a0418410e35586ac8bd7c86af9d9f
prince-singh19 <princesingh9621680755@gmail.com>
2025-07-10 03:03:53 +0530

    change frontent url

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000" , "https://electyfy1-frontend.vercel.app"],
+    origin: ["http://localhost:3000" , "https://electyfy-frontent.vercel.app"],
   })
 );
 
